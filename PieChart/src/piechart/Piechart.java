724d67cbc762664b2d08993f167c1b4709541b9e
Michal Sobocinski <msobocin@jaumebalmes.net>
2015-02-14 17:11:26 +0100

    JFrame

--- PieChart/src/piechart/Piechart.java
+++ PieChart/src/piechart/Piechart.java
@@ -1,8 +1,15 @@
 package piechart;
 
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 import java.util.HashMap;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartFrame;
@@ -11,7 +18,9 @@ import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PiePlot3D;
 import org.jfree.data.general.DefaultPieDataset;
 
-public class Piechart {
+public class Piechart extends JFrame implements ActionListener{
+	private JButton[] b = new JButton[3];
+	
 	public void PieChart(HashMap<String, Double> mapa) {
 		DefaultPieDataset pieDataset = new DefaultPieDataset();
         for (String s : mapa.keySet()) {
@@ -26,10 +35,46 @@ public class Piechart {
 //        frame1.setSize(300, 300);
         ChartPanel panel = new ChartPanel(chart);
         JFrame frame = new JFrame("Michal Sobocinski Grafic Extensions");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JButton[] b = new JButton[3];
+        b[0] = new JButton("Select");
+        b[1] = new JButton("X+");
+        b[2] = new JButton("Y+");
+        frame.setLayout(new GridLayout(1, 3));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(panel);
+        
+         //grid layout per a taules
+        for (int i = 0; i < 3; i++) {
+            frame.add(b[i]);
+            b[i].addActionListener(this); //per controlar els events sobre el boto
+            //s'ha d'implementar l'escolta dels events
+        }
+        frame.setPreferredSize(new Dimension(640,480));
         frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+        for (int i = 0; i < 3; i++) {
+        		System.out.println("source: "+e.getSource());
+        		System.out.println("przycisk: "+b[0]);
+            if (e.getSource() == b[i]) {
+				switch (i) {
+                    case 0:
+	                    	 Select ventana = new Select();
+	                		 File file = ventana.getArchivo();
+                        break;
+                    case 1:
+                    		System.out.println("hola");
+                        break;
+                    case 2:
+                    		System.out.println("hola");
+                        break;
+                }
+            }
+        }
 	}
 }
