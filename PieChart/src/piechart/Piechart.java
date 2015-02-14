9f3ca75008713a93beeb3a65a00f8797bb4f3297
Michal Sobocinski <msobocin@jaumebalmes.net>
2015-02-14 16:22:05 +0100

    PieChart

--- PieChart/src/piechart/Piechart.java
+++ PieChart/src/piechart/Piechart.java
@@ -2,8 +2,11 @@ package piechart;
 
 import java.util.HashMap;
 
+import javax.swing.JFrame;
+
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartFrame;
+import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PiePlot3D;
 import org.jfree.data.general.DefaultPieDataset;
@@ -19,7 +22,14 @@ public class Piechart {
         PiePlot3D p = (PiePlot3D) chart.getPlot();
         p.setForegroundAlpha(0.5f);
         ChartFrame frame1 = new ChartFrame("Michal Sobocinski Grafic Extensions", chart);
-        frame1.setVisible(true);
-        frame1.setSize(300, 300);
+//        frame1.setVisible(true);
+//        frame1.setSize(300, 300);
+        ChartPanel panel = new ChartPanel(chart);
+        JFrame frame = new JFrame("Michal Sobocinski Grafic Extensions");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(panel);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
 	}
 }
--- PieChart/src/piechart/main.java
+++ PieChart/src/piechart/main.java
@@ -1,7 +1,15 @@
 package piechart;
 
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.util.HashMap;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class main {
 
@@ -9,7 +17,7 @@ public class main {
 
 		Select ventana = new Select();
 		File file = ventana.getArchivo();
-		
+        
 		Recorrer recorrer = new Recorrer();
 		HashMap<String, Double> extensions = recorrer.recorregutPreordre(file);
 		
